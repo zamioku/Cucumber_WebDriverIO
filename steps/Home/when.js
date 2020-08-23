353d66fcd5d2b9950d8f5b2aaf8c340329a0e22e
zamioku <zamioku@github.com>
2020-08-23 15:00:20 +0300

    Quick clean-up

--- steps/Home/when.js
+++ steps/Home/when.js
@@ -1,14 +1,9 @@
 import { When } from "cucumber";
 import search from "../../support/actions/search";
-import selectMenuOption from "../../support/actions/selectMenuOption";
 import waitForPageToLoad from "../../support/actions/waitForPageToLoad";
 
 When(/^the user searches for "(.*)"$/, keyword => {
     search(keyword);
-});
-
-When(/^the user clicks on the "(.*)" menu option in the menu$/, option => {
-    selectMenuOption(option);
 });
 
 When(/^the page is loaded$/, () => {    
